6447a738df1b452750ee17cbb92dd7e80f65fed9
Nourhan Ahmed <46425742+nourhanahmedelbadawie@users.noreply.github.com>
2024-10-04 20:29:33 +0300

    python_introduction

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10;
+number2 = 5;
+sum = number1 + number2;
+print('sum of' ,number1 ,'and', number2, 'is', sum)
+differance = number1 - number2;
+print('differance of' ,number1 ,'and', number2, 'is', differance)
+
+product = number1 * number2
+print('product of' ,number1 ,'and', number2, 'is', product)
