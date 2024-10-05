1ffd9b988635413f23e03cc9439b32b5fffb886b
Nourhan Ahmed <46425742+nourhanahmedelbadawie@users.noreply.github.com>
2024-10-05 21:24:24 +0300

    fix

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -1,8 +1,11 @@
 import arithmetic_operations
 print("Arithmetic Operations")
-num1 = float(input('Enter the first number: '))
-num2 = float(input('Enter the second number: '))
-operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+def main():
+ num1 = float(input('Enter the first number: '))
+ num2 = float(input('Enter the second number: '))
+ operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
-result = arithmetic_operations.perform_operation(num1 ,  num2 , operation )
-print('Result: ' ,result)
+ result = arithmetic_operations.perform_operation(num1 ,  num2 , operation )
+ print('Result: ' ,result)
+
+main()
