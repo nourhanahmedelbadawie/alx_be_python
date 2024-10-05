7c5980a35b2c63754b810b7dd8be8d744d746ddc
Nourhan Ahmed <46425742+nourhanahmedelbadawie@users.noreply.github.com>
2024-10-05 20:11:36 +0300

    arithmetic_operations

---
+++ fns_and_dsa/main.py
@@ -0,0 +1,7 @@
+import arithmetic_operations
+
+num1 = float(input('Enter the first number: '))
+num2 = float(input('Enter the second number: '))
+operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+result = arithmetic_operations.perform_operation(num1 ,  num2 , operation )
