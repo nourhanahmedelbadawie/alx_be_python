4cc99ccc24da71e9d854850a6bd3e35bb8b28f9d
Nourhan Ahmed <46425742+nourhanahmedelbadawie@users.noreply.github.com>
2024-10-05 20:14:27 +0300

    fix

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -5,3 +5,4 @@ num2 = float(input('Enter the second number: '))
 operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
 result = arithmetic_operations.perform_operation(num1 ,  num2 , operation )
+print('Result: ' ,result)
