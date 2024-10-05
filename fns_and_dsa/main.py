c0584e963641f249e979e5abb1ae9cf61ab0db6e
Nourhan Ahmed <46425742+nourhanahmedelbadawie@users.noreply.github.com>
2024-10-05 21:30:40 +0300

    fix

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -5,7 +5,8 @@ def main():
  num2 = float(input('Enter the second number: '))
  operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
- result = perform_operation(num1 ,  num2 , operation )
- print('Result: ' ,result)
+
+ result = perform_operation(num1, num2, operation)
+ print(f"Result: {result}")
 
 main()
