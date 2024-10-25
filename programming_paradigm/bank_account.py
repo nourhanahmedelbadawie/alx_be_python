3572bee29c7dc02f6bebfa38f35869b4f3caa79c
nourhanahmedelbadawie <nourhanahmedelbadawie@gmail.com>
2024-10-25 20:46:54 +0300

    return

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,19 +1,20 @@
 class BankAccount:
-    def __init__(self, account_balance=0):
-        # Initialize with the provided balance, default to 0 if not given.
-        self.account_balance = account_balance
+    def __init__(self, account_balance ):
+      
+        self.account_balance  = 0  # Default attribute value
 
-    def deposit(self, amount):
-        self.account_balance += amount
-        print(f"Deposited: ${amount:.2f}")
+    def deposit(self ,amount):
+        self.account_balance = self.account_balance + amount
+
 
     def withdraw(self, amount):
         if self.account_balance >= amount:
-            self.account_balance =  self.account_balance- amount
-            print(f"Withdrew: ${self.account_balance}")
+            self.account_balance -= amount
+            print(f"Withdrew: ${amount:.2f}")
         else:
             print("Insufficient funds.")
 
+
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f" Current Balance:${self.account_balance} .")
 
