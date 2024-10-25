e574156f406bb99d068d193b4917a6b94c44961b
nourhanahmedelbadawie <nourhanahmedelbadawie@gmail.com>
2024-10-25 19:54:32 +0300

    programming_paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self, account_balance ):
+      
+        self.account_balance  = 0  # Default attribute value
+
+    def deposit(self ,amount):
+        self.account_balance = self.account_balance + amount
+
+
+    def withdraw(self ,amount):
+        res = self.account_balance - amount
+        if res < 0:
+         return False
+        else:
+         return True
+
+    def display_balance(self):
+        print(f" blance is {self.account_balance} .")
+
