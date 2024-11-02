69ca2d4811722faebb86ccba6f553c77d42ab3a3
nourhanahmedelbadawie <nourhanahmedelbadawie@gmail.com>
2024-11-02 19:23:31 +0200

    add del method

--- oop/book_class.py
+++ oop/book_class.py
@@ -10,4 +10,8 @@ class Book:
     def __repr__(self):
      return f"Book('{self.title}', '{self.author}', {self.year})"
     
+    def __del__(self):
+        # Cleanup code here
+        print(f"Deleting '{self.title}'")
+    
 
