f057f47bea93c0c17b07cb9985827794f62113a8
nourhanahmedelbadawie <nourhanahmedelbadawie@gmail.com>
2024-11-02 20:20:31 +0200

    polymorphism_demo

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,18 @@
+import math
+
+class Shape:
+
+   def __init__(self , area):
+     self.area = area
+      
+
+     class Rectangle:
+       def __init__(self ,  length , width):
+        self.area = length * width
+      
+
+
+
+        class Circle:
+                 def __init__(self ,  radius ):
+                  self.area = math.pi * (radius ** 2)
