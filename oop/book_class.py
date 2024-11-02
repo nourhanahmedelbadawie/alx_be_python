3549da1aa7464c6956dc066bd05f6b334d3d3ab1
nourhanahmedelbadawie <nourhanahmedelbadawie@gmail.com>
2024-11-02 19:26:43 +0200

    fix

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,17 +1,14 @@
 class Book:
-  def __init__(self, title  , author  , year ):
-    self.title = title
-    self.author = author
-    self.year = year
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
 
     def __str__(self):
-     return f"{self.title} by {self.author} , published in {self.year} "
+        return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-     return f"Book('{self.title}', '{self.author}', {self.year})"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
     
     def __del__(self):
-        # Cleanup code here
         print(f"Deleting '{self.title}'")
-    
-
